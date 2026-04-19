db6749786a7a8dad429a34dff3041d36f9eabd31
Ryan Tan <ryantan.mail@gmail.com>
2026-04-19 11:51:43 -0700

    update alu

--- src/alu.h
+++ src/alu.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <cstdint>
 
+struct ALUResult {
+	uint16_t value;
+	bool zero;
+	bool negative;
+};
+
 class ALU {
-	
-	public:
+	public: 
+		ALUResult execute(uint16_t opCode, uint16_t a, uint16_t b);
+	private:
 		// Wrapper around operations, seem silly but it's kept for extendability 
 		uint16_t add(uint16_t a, uint16_t b) {
 			return a + b;
@@ -12,4 +19,6 @@ class ALU {
 		uint16_t sub(uint16_t a, uint16_t b) {
 			return a - b;
 		}
+
+
 };
