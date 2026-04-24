c55ce84ae90222d68cfefa8083ca35f5db0651d3
Ryan Tan <ryantan.mail@gmail.com>
2026-04-24 16:06:02 -0700

    left out a new test_clock file

---
+++ test/test_clock.cpp
@@ -0,0 +1,20 @@
+#include "../include/clock.h"
+#include "micro_test_framework.h"
+
+class TestClockSuite : public TestSuite {
+
+public:
+  void runTests() {
+    executeTest("clock ticking", [this] { this->testClockTick(); });
+  }
+  void onMount() { std::cout << "\n[SUITE] Test Clock Begin\n"; }
+  void onDismount() { std::cout << "\n[SUITE] Test Clock End\n"; }
+
+private:
+  // Pretty trivial but test coverage is nice
+  void testClockTick() {
+    Clock clock;
+    test_assert(clock.tick());
+    test_assert(clock.getTime() == 1);
+  }
+};
